a51a19a0c3ce8c7fb84ee126fce8f63bbb37a5bc
Braxton Toomer <braxton.toomer1@gmail.com>
2020-05-21 23:59:34 -0700

    structure dom elements into specific functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-//    Constants:
+// Constants:
 
 
-//    Variables:
+// Variables:
 
 
-//    Cached Element References:
+// Cached Element References:
 const container = document.querySelector('.container');
 const title = document.querySelector('.title');
 const background = document.querySelector('.background');
@@ -15,45 +15,99 @@ const operations = document.querySelector('.operations');
 const miscBtns = document.querySelector('.misc-btns');
 const footer = document.querySelector('.footer');
 
-//    Created DOM Elements:
-const header = document.createElement('h1');
-const digits = document.createElement('button');
-const addBtn = document.createElement('button');
-const subBtn = document.createElement('button');
-const mulBtn = document.createElement('button');
-const divBtn = document.createElement('button');
-const negBtn = document.createElement('button');
-const eraseBtn = document.createElement('button');
-const equalBtn = document.createElement('button');
 
-//    Append Elements to Parent:
-title.appendChild(header);
+// Append Elements to Parent:
 calculatorContainer.appendChild(displayOutput);
 calculatorContainer.appendChild(keypad);
 calculatorContainer.appendChild(operations);
 calculatorContainer.appendChild(miscBtns);
-keypad.appendChild(digits);
-keypad.appendChild(negBtn);
-operations.appendChild(addBtn);
-operations.appendChild(subBtn);
-operations.appendChild(mulBtn);
-operations.appendChild(divBtn);
-miscBtns.appendChild(eraseBtn);
-miscBtns.appendChild(equalBtn);
-
-//    Event Listener:
-document.addEventListener('DOMContentLoaded', function () {
 
 
-});
+// Event Listener:
+document.addEventListener('DOMContentLoaded', createKeyPad);
 
 
-//    Functions:
-
-function loadPage() {
-  add_button.innerText = '+';
-
+// Functions:
+function createKeyPad() {
+  for (let i = 0; i < 10; i++) {
+    const digits = document.createElement('button');
+    digits.setAttribute('class', `number num_${i}`);
+    digits.setAttribute('value', i);
+    digits.innerText = i;
+    addEventListener(digits);
+    keypad.appendChild(digits);
+    createMiscBtns();
+  }
 }
+
+function createMiscBtns() {
+  const equalBtn = document.createElement('button');
+  equalBtn.setAttribute('class', 'misc equal');
+  equalBtn.setAttribute('value', 'equal');
+  equalBtn.innerText = '=';
+  addEventListener(equalBtn);
+  miscBtns.appendChild(equalBtn);
+
+  const clearBtn = document.createElement('button');
+  clearBtn.setAttribute('class', 'misc clear');
+  clearBtn.setAttribute('value', 'clear');
+  clearBtn.innerText('C');
+  addEventListener(clearBtn);
+  miscBtns.appendChild(clearBtn);
+  createAddBtn();
+}
+
+function createAddBtn() {
+  const addBtn = document.createElement('button');
+  addBtn.setAttribute('class', 'input');
+  addBtn.setAttribute('value', 'add');
+  addBtn.innerText = '+';
+  addEventListener(addBtn);
+  operations.appendChild(addBtn);
+  createSubBtn();
+}
+
+function createSubBtn() {
+  const subBtn = document.createElement('button');
+  subBtn.setAttribute('class', 'input');
+  subBtn.setAttribute('value', 'sub');
+  subBtn.innerText = '-';
+  addEventListener(subBtn);
+  operations.appendChild(subBtn);
+  createMulBtn();
+}
+
+function createMulBtn() {
+  const mulBtn = document.createElement('button');
+  mulBtn.setAttribute('class', 'input');
+  mulBtn.setAttribute('value', 'mult');
+  mulBtn.innerText = '+';
+  addEventListener(mulBtn);
+  operations.appendChild(mulBtn);
+  createDivBtn();
+}
+
+function createDivBtn() {
+  const divBtn = document.createElement('button');
+  divBtn.setAttribute('class', 'input');
+  divBtn.setAttribute('value', 'div');
+  divBtn.innerText = '+';
+  addEventListener(divBtn);
+  operations.appendChild(divBtn);
+  createPlusMinusBtn();
+}
+
+function createPlusMinusBtn() {
+  const plusminusBtn = document.createElement('button');
+  plusminusBtn.setAttribute('class', 'input neg');
+  plusminusBtn.setAttribute('value', 'negative')
+  plusminusBtn.innerText = '+/-';
+  addEventListener(plusminusBtn);
+  keypad.appendChild(negBtn);
+}
+
+
+
 
 
 
